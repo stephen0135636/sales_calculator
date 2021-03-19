da996dbeb4651fddd3392718e813711e2c616fdf
nonso <nonso@me.com>
2021-03-19 03:48:23 +0100

    setting up uploads

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 var express = require("express");
 var app = express();
 
+const port = process.env.PORT || 3000
+
 const publicDirectoryPath = path.join(__dirname, './public')
 
 app.use(express.static(publicDirectoryPath))
@@ -12,7 +14,7 @@ app.get("" , (req, res) => {
     res.render('index')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server running on port 3000");
 });
    
